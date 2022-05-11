6bea021f4d99eaff35e9ecea3578bb5a6941737b
Rob Petrungaro <rob.petrungaro@gmail.com>
2022-05-11 14:40:09 -0400

    added script file, wrote out pseudo code

---
+++ script.js
@@ -0,0 +1,19 @@
+// add a submit event listener on the form
+
+    // stop the page from refreshing when the form is submitted
+
+    // only if the user has entered text:
+
+    // grab the users input from the form
+    // create a li element and store the users input as text inside the li
+        // include a checkbox icon within the li
+    // put that li in the existing ul on the page
+
+// clicking on a task allows you to toggle between checked/unchecked (done  not done)
+
+// BONUSES
+// add a 'reset' button which clears all the todo's
+// add a 'remove task' button to each task
+// add a congratulations alert when all the existing todos are checked off
+// add a 'take a break' message after 5 tasks are completed
+// add an 'edit task' button 
